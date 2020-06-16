1ae22cd27f6e168cbe7373323b5e6f8d48d29026
darnocer <darnocer@gmail.com>
2020-06-15 20:05:21 -0600

    initial connection and dependencies

---
+++ server.js
@@ -0,0 +1,23 @@
+// DEPENDENCIES
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const console_table = require("console.table");
+const chalk = require("chalk");
+const clear = require("console-clear");
+
+const log = console.log;
+
+// creates the connection information for the sql database
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "employees_DB",
+});
+
+// connects to the mysql server and sql database
+connection.connect(function (err) {
+  if (err) throw err;
+  start();
+});
