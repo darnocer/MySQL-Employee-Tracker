e8aa6db8a99b556c6a5df9ad45338b89ad2d73e9
darnocer <darnocer@gmail.com>
2020-06-15 20:30:36 -0600

    prompt functions and exit

--- server.js
+++ server.js
@@ -42,11 +42,17 @@ function promptUser() {
       choices: [
         "View All Employees",
         "View All Employees By Department",
-        "View All Employees By Manager",
+        "View All Employees By Role",
+        // "View All Employees By Manager",
         "Add Employee",
-        "Remove Employee",
+        "Add Role",
+        "Add Department",
+        // "Remove Employee",
+        // "Remove Department",
+        // "Remove Role",
         "Update Employee Role",
-        "Update Employee Manager",
+        // "Update Employee Manager",
+        "Exit",
       ],
     })
     .then((answer) => {
@@ -55,9 +61,95 @@ function promptUser() {
           viewEmployees();
           break;
       }
+      switch (answer.userSelection) {
+        case "View All Employees By Department":
+          viewByDepartment();
+          break;
+      }
+      // switch (answer.userSelection) {
+      //   case "View All Employees By Manager":
+      //     viewByManager();
+      //     break;
+      // }
+      switch (answer.userSelection) {
+        case "Add Employee":
+          addEmployee();
+          break;
+      }
+      switch (answer.userSelection) {
+        case "Add Role":
+          addRole();
+          break;
+      }
+      switch (answer.userSelection) {
+        case "Add Department":
+          addDepartment();
+          break;
+      }
+      // switch (answer.userSelection) {
+      //   case "Remove Employee":
+      //     removeEmployee();
+      //     break;
+      // }
+      // switch (answer.userSelection) {
+      //   case "Remove Role":
+      //     removeRole();
+      //     break;
+      // }
+      // switch (answer.userSelection) {
+      //   case "Remove Department":
+      //     removeDepartment();
+      //     break;
+      // }
+      switch (answer.userSelection) {
+        case "Update Employee Role":
+          updateEmployee();
+          break;
+      }
+      switch (answer.userSelection) {
+        case "Exit":
+          connection.end();
+          break;
+      }
     });
 }
 
 function viewEmployees() {
-  console.log("HELLO");
+  log("Viewing All Employees");
+}
+
+function viewByDepartment() {
+  log("Viewing By Department");
+}
+
+// function viewByManager() {
+//   log("Viewing By Manager");
+// }
+
+function addEmployee() {
+  log("Adding employee");
+}
+
+function addRole() {
+  log("Adding Role");
+}
+
+function addDepartment() {
+  log("Adding Role");
+}
+
+// function removeEmployee() {
+//   log("Removing Employee");
+// }
+
+// function removeRole() {
+//   log("Removing Role");
+// }
+
+// function removeDepartment() {
+//   log("Removing Department");
+// }
+
+function updateEmployee() {
+  log("Updating Employee");
 }
